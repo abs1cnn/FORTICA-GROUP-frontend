beb8940c5c9fadc1dee8dbe2f2a3ee6f926c5403
abs_cnn <abigailcanaan@gmail.com>
2024-12-13 12:03:59 -0300

    hosteando3.1

--- src/Footer/Footer.jsx
+++ src/Footer/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
     return (
         <footer>
             <div className="footer-content">
-                <p>&copy; 2024 Mi Empresa. Todos los derechos reservados.</p>
+                <p>&copy; 2024 Fortica. Todos los derechos reservados.</p>
                 <ul className="social-links">
                     <li><a href="#" target="_blank" rel="noopener noreferrer">Facebook</a></li>
                     <li><a href="#" target="_blank" rel="noopener noreferrer">Twitter</a></li>
